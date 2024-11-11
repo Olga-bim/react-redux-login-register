b811cc7cee72b0718a65c99bc7c054b43d279d73
Olga <olga.bimstart@gmail.com>
2024-11-12 01:45:42 +0200

    backend/base

--- backend/base/wsgi.py
+++ backend/base/wsgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')  # Убедитесь, что здесь указано ваше приложение
+# Указываем правильный путь к настройкам
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.base.settings')  # Путь с учетом папки backend
 application = get_wsgi_application()
