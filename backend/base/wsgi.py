f97256090bd2a388de28be69fa3276c364617d38
Olga <olga.bimstart@gmail.com>
2024-11-12 01:39:59 +0200

    https://react-redux-login-register.onrender.com

--- backend/base/wsgi.py
+++ backend/base/wsgi.py
@@ -1,6 +1,5 @@
-
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')  # Замените base на имя вашего приложения
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')  # Убедитесь, что здесь указано ваше приложение
 application = get_wsgi_application()
