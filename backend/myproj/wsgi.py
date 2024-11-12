1568afb6e93343f2e038edd08f768a609f35450f
Olga <olga.bimstart@gmail.com>
2024-11-12 02:18:27 +0200

    change wgsi Procfile3

--- backend/myproj/wsgi.py
+++ backend/myproj/wsgi.py
@@ -1,7 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Указываем путь к файлу настроек в папке backend/myproj
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.myproj.settings')
-
 application = get_wsgi_application()
